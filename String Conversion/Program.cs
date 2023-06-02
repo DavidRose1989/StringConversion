ac6ce183edfe27cf7023a2218ecafd7cbc3cddc6
DavidRose <DavidRose331@Gmail.com>
2023-06-02 09:48:07 -0500

    initial

---
+++ String Conversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StingConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string mySecondString = "15";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int resultint = num1 + num2;
+           
+
+            Console.WriteLine(resultint);
+            Console.Read();
+
+        }
+    }
+}
